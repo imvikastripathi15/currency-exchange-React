1da642c52fa8fe533c86efba4e1e265a0438cefa
imvikastripathi15 <funnyvikki15@gmail.com>
2024-04-30 19:12:43 +0530

    bg img added

--- currency-exchange/src/App.jsx
+++ currency-exchange/src/App.jsx
@@ -29,7 +29,9 @@ const convertValue = ()=>{
 return (
     <div
         className="w-full h-screen flex flex-wrap justify-center items-center bg-cover bg-no-repeat"
-      
+        style={{
+            backgroundImage: `url('https://images.pexels.com/photos/106152/euro-coins-currency-money-106152.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2 ')`,
+        }}
     >
         <div className="w-full">
             <div className="w-full max-w-md mx-auto border border-gray-60 rounded-lg p-5 backdrop-blur-sm bg-white/30">
